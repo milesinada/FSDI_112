9eb535f04430c0c25ee362c376205b8dd4189d3f
Miles Inada <milesinada219@gmail.com>
2022-03-17 21:04:32 -1000

    Static Files

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from tempfile import tempdir
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pages.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import HomePageView, AboutPageView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
